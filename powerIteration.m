fd6298d7b2bab7589daf3f5d527f3f427115993b
Howard Jing <howard.jing@gmail.com>
2011-12-22 21:27:49 -0500

    power iteration

---
+++ powerIteration.m
@@ -0,0 +1,14 @@
+function [domVal domVec] = powerIteration(A, appx)
+
+    counter = 0;
+    epsilon = 0.001;
+    error = 100;
+    while (counter < 10000) && (error > epsilon)
+        temp = appx;
+        appx = A*appx / norm(A*appx, 2);
+        error = norm(temp-appx,2);
+        counter = counter + 1;
+    end
+    domVec = appx;
+    domVal = appx'*A*appx;
+end
